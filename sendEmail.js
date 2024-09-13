e9eb6c8887e3cc245649f9f0f31dbcf1d4bf43ac
Maxim-LD <ayinde779@gmail.com>
2024-09-13 06:37:47 +0100

    added functionality

--- sendEmail.js
+++ sendEmail.js
@@ -1,28 +1,42 @@
 const nodemailer = require('nodemailer')
-
+const Clients = require("./model/authModel")
+const generateCertificate = require('./generateCertificate')
+require('dotenv').config()
 
 
 const sendEmail = async (email)=>{
 
    try {
-    
+
+        const logUser = await Clients.findOne({ email })
+
+        const name = `${logUser.firstName} ${logUser.lastName}`
+
+        const filePath = await generateCertificate(name);
+
         const mailTransport = nodemailer.createTransport({
 
             service: "gmail",
             auth: {
 
-                email: "ayinde779@gmail.com",
-                password: "kvjo habg diww fwde"
+                user: `${process.env.EMAIL}`,
+                pass: `${process.env.EMAIL_PASS}`
             }
         })
 
         const sentObject = {
-            from: "ayinde779@gmail.com",
+            from: process.env.EMAIL,
             to: email,
             subject: "CERTIFICATE OF ENROLLMENT",
-            text: "Thanks for enrolling!" 
+            text: "Thanks for enrolling!",
+            attachments: [
+                {
+                    filename: `${name}-certificate.pdf`,
+                    path: filePath,
+                    contentType: 'application/pdf'
+                }
+            ] 
         
-
         }
 
         const result = await mailTransport.sendMail(sentObject)
